52de51d6264cb1906ace591f95f2412063360d95
Fabio Insolia <fabio.insolia@gmail.com>
2022-08-15 20:27:17 +0100

    Dagger hilt implementation

---
+++ app/src/main/java/com/fabio/weatherapp/WeatherApp.kt
@@ -0,0 +1,7 @@
+package com.fabio.weatherapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WeatherApp: Application()
---
+++ app/src/main/java/com/fabio/weatherapp/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.fabio.weatherapp.di
+
+import com.fabio.weatherapp.feature_weather.data.repository.WeatherRepositoryImpl
+import com.fabio.weatherapp.feature_weather.domain.repository.WeatherRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRepository(weatherRepositoryImpl: WeatherRepositoryImpl): WeatherRepository
+}
